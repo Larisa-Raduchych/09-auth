0f89aa2f2e9561627467b4c8c577b89f4b89cb91
Lari Lari <larisaraduchych@gmail.com>
2026-08-16 19:03:31 +0300

    change base url

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const nextApi = axios.create({
-  baseURL: "http://localhost:3000/api",
-  // `${process.env.NEXT_PUBLIC_API_URL}/api`
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
   withCredentials: true,
 });
